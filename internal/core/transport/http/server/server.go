3c6ac19b4b0b7e3b0f3a39c2a61841f12418a1b7
nsvaulin <mr.vau2017@mail.ru>
2026-04-14 16:27:47 +0500

    feat: impl server.go

--- internal/core/transport/http/server/server.go
+++ internal/core/transport/http/server/server.go
@@ -1,3 +1,67 @@
 package core_http_server
 
-// type HTTPServer
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+
+	core_logger "github.com/nikitavaulin/kudesnik/internal/core/logger"
+	"go.uber.org/zap"
+)
+
+type HTTPServer struct {
+	mux    *http.ServeMux
+	config *HTTPServerConfig
+	log    *core_logger.Logger
+}
+
+func NewHTTPServer(config *HTTPServerConfig, log *core_logger.Logger) *HTTPServer {
+	return &HTTPServer{
+		mux:    http.NewServeMux(),
+		config: config,
+		log:    log,
+	}
+}
+
+func (h *HTTPServer) Run(ctx context.Context) error {
+	server := &http.Server{
+		Addr:    h.config.Address,
+		Handler: h.mux,
+	}
+
+	ch := make(chan error, 1)
+
+	go func() {
+		defer close(ch)
+
+		h.log.Warn("start HTTP server", zap.String("address", h.config.Address))
+
+		err := server.ListenAndServe()
+
+		if !errors.Is(err, http.ErrServerClosed) {
+			ch <- err
+		}
+	}()
+
+	select {
+	case err := <-ch:
+		if err != nil {
+			return fmt.Errorf("listen and serve http server: %w", err)
+		}
+	case <-ctx.Done():
+		h.log.Warn("shutdown HTTP server...")
+
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), h.config.ShutdownDuration)
+		defer cancel()
+
+		if err := server.Shutdown(shutdownCtx); err != nil {
+			_ = server.Close()
+			return fmt.Errorf("shutdown HTTP server: %w", err)
+		}
+
+		h.log.Warn("HTTP server stopped successfully")
+	}
+
+	return nil
+}
