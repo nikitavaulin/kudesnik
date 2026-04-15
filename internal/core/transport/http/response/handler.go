be7a862db3fbf489b020ab1afae589aa75e9a953
nsvaulin <mr.vau2017@mail.ru>
2026-04-15 22:57:09 +0500

    feat: add ErrorResponse and core_errors

--- internal/core/transport/http/response/handler.go
+++ internal/core/transport/http/response/handler.go
@@ -2,9 +2,11 @@ package core_http_response
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 
+	core_errors "github.com/nikitavaulin/kudesnik/internal/core/errors"
 	core_logger "github.com/nikitavaulin/kudesnik/internal/core/logger"
 	"go.uber.org/zap"
 )
@@ -21,10 +23,41 @@ func NewHTTPReponseHandler(log *core_logger.Logger, rw http.ResponseWriter) *HTT
 	}
 }
 
+func (h *HTTPReponseHandler) ErrorResponse(err error, msg string) {
+	var statusCode int
+	var logFunc func(string, ...zap.Field)
+
+	switch {
+	case errors.Is(err, core_errors.ErrNotFound):
+		statusCode = http.StatusNotFound
+		logFunc = h.log.Debug
+
+	case errors.Is(err, core_errors.ErrInvalidArgument):
+		statusCode = http.StatusBadRequest
+		logFunc = h.log.Warn
+
+	case errors.Is(err, core_errors.ErrConflict):
+		statusCode = http.StatusConflict
+		logFunc = h.log.Warn
+
+	default:
+		statusCode = http.StatusInternalServerError
+		logFunc = h.log.Error
+	}
+
+	logFunc(msg, zap.Error(err))
+	h.errorResponse(statusCode, err, msg)
+
+}
+
 func (h *HTTPReponseHandler) PanicResponse(p any, msg string) {
 	statusCode := http.StatusInternalServerError
 	err := fmt.Errorf("unexpected panic: %v", p)
 
+	h.errorResponse(statusCode, err, msg)
+}
+
+func (h *HTTPReponseHandler) errorResponse(statusCode int, err error, msg string) {
 	h.log.Error(msg, zap.Error(err))
 	h.rw.WriteHeader(statusCode)
 
@@ -36,5 +69,4 @@ func (h *HTTPReponseHandler) PanicResponse(p any, msg string) {
 	if err := json.NewEncoder(h.rw).Encode(response); err != nil {
 		h.log.Error("write HTTP response", zap.Error(err))
 	}
-
 }
