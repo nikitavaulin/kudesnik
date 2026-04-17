92f69c9165c12fdbebd68dec962829216870ece1
nsvaulin <mr.vau2017@mail.ru>
2026-04-17 18:22:42 +0500

    feat: add pgxpool and config in core

---
+++ internal/core/repository/postgres/pool/pool.go
@@ -0,0 +1,51 @@
+package core_postgres_pool
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Pool interface {
+	Exec(ctx context.Context, sql string, arguments ...any) (pgconn.CommandTag, error)
+	Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
+	Close()
+}
+
+type ConnectionPool struct {
+	pool   *pgxpool.Pool
+	opTime time.Duration
+}
+
+func NewConnectionPool(ctx context.Context, config Config) (*ConnectionPool, error) {
+	connectionString := fmt.Sprintf(
+		"postgresql://%s:%s@%s:%s/%s?sslmode=disable",
+		config.User, config.Password,
+		config.Host, config.Port,
+		config.Database,
+	)
+
+	pgxcfg, err := pgxpool.ParseConfig(connectionString)
+	if err != nil {
+		return nil, fmt.Errorf("parse pgxpool cfg: %w", err)
+	}
+
+	pool, err := pgxpool.NewWithConfig(ctx, pgxcfg)
+	if err != nil {
+		return nil, fmt.Errorf("create pgxpool: %w", err)
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		return nil, fmt.Errorf("ping pgxpool: %w", err)
+	}
+
+	return &ConnectionPool{
+		pool:   pool,
+		opTime: config.Timeout,
+	}, nil
+}
