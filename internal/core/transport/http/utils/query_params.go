fa93faa1b04e429d6d0a7619b2745a34b7cb1b0a
nsvaulin <mr.vau2017@mail.ru>
2026-04-19 20:40:44 +0500

    feat: create core_http_utils and add getting query params

---
+++ internal/core/transport/http/utils/query_params.go
@@ -0,0 +1,27 @@
+package core_http_utils
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	core_errors "github.com/nikitavaulin/kudesnik/internal/core/errors"
+)
+
+func GetIntQueryParam(request *http.Request, key string) (*int, error) {
+	value := request.URL.Query().Get(key)
+	if value == "" {
+		return nil, nil
+	}
+
+	intValue, err := strconv.Atoi(value)
+	if err != nil {
+		return nil, fmt.Errorf(
+			"param: %s with key: %s could not be convert to integer: %v: %w",
+			value, key,
+			err, core_errors.ErrInvalidArgument,
+		)
+	}
+
+	return &intValue, nil
+}
