d46a00e840e485ccf00a57abe219922c5e1d18cc
nsvaulin <mr.vau2017@mail.ru>
2026-04-30 16:52:31 +0500

    refactor: add validatable interface and change DecodeAndValidateRequest

--- internal/core/transport/http/request/decode.go
+++ internal/core/transport/http/request/decode.go
@@ -11,12 +11,24 @@ import (
 
 var requestValidator = validator.New()
 
+type validatable interface {
+	Validate() error
+}
+
 func DecodeAndValidateRequest(request *http.Request, dest any) error {
 	if err := json.NewDecoder(request.Body).Decode(&dest); err != nil {
 		return fmt.Errorf("decode json: %v: %w", err, core_errors.ErrInvalidArgument)
 	}
 
-	if err := requestValidator.Struct(dest); err != nil {
+	var err error
+
+	v, ok := dest.(validatable)
+	if ok {
+		err = v.Validate()
+	} else {
+		err = requestValidator.Struct(dest)
+	}
+	if err != nil {
 		return fmt.Errorf("request validation: %v: %w", err, core_errors.ErrInvalidArgument)
 	}
 
