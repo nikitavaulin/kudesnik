60e84ef7e5d54f1bf99bc43a523ac05ec9f81358
nsvaulin <mr.vau2017@mail.ru>
2026-04-30 16:50:14 +0500

    refactor:  validation in product_categories

--- internal/core/domain/product_categories.go
+++ internal/core/domain/product_categories.go
@@ -33,11 +33,11 @@ func NewProductCategory(id uuid.UUID, name string, installationPrice float64) *P
 
 func (c *ProductCategory) Validate() error {
 	nameLength := len([]rune(c.CategoryName))
-	if !core_validation.IsIntInBounds(nameLength, MinProductCategoryNameLength, MaxProductCategoryNameLength) {
+	err := core_validation.ValidateIntInBounds(nameLength, MinProductCategoryNameLength, MaxProductCategoryNameLength)
+	if err != nil {
 		return fmt.Errorf(
-			"product category should be between %d and %d, got: %d: %w",
-			MinProductCategoryNameLength, MaxProductCategoryNameLength,
-			nameLength,
+			"invalid category name: %v: %w",
+			err,
 			core_errors.ErrInvalidArgument,
 		)
 	}
@@ -49,6 +49,42 @@ func (c *ProductCategory) Validate() error {
 			core_errors.ErrInvalidArgument,
 		)
 	}
+
+	return nil
+}
+
+type ProductCategoryPatch struct {
+	CategoryName      Nullable[string]
+	InstallationPrice Nullable[float64]
+}
+
+func (c *ProductCategoryPatch) Validate() error {
+	if c.CategoryName.Set && c.CategoryName.Value == nil {
+		return fmt.Errorf("CategoryName can't be patched to NULL: %w", core_errors.ErrInvalidArgument)
+	}
+	return nil
+}
+
+func (c *ProductCategory) ApplyPatch(patch ProductCategoryPatch) error {
+	if err := patch.Validate(); err != nil {
+		return fmt.Errorf("invalid product category patch: %w", err)
+	}
+
+	tmp := *c
+
+	if patch.CategoryName.Set {
+		tmp.CategoryName = *patch.CategoryName.Value
+	}
+
+	if patch.InstallationPrice.Set {
+		tmp.InstallationPrice = *patch.InstallationPrice.Value
+	}
+
+	if err := tmp.Validate(); err != nil {
+		return fmt.Errorf("ivalid patched product category: %w", err)
+	}
+
+	*c = tmp
 
 	return nil
 }
