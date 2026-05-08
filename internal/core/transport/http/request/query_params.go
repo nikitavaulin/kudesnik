0404ffa01fb89cf000ae8d4d7201716887e856b9
nsvaulin <mr.vau2017@mail.ru>
2026-05-08 22:46:04 +0500

    feat: add get query params uuid limit offset to core

--- internal/core/transport/http/request/query_params.go
+++ internal/core/transport/http/request/query_params.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/google/uuid"
 	core_errors "github.com/nikitavaulin/kudesnik/internal/core/errors"
 )
 
@@ -24,4 +25,36 @@ func GetIntQueryParam(request *http.Request, key string) (*int, error) {
 	}
 
 	return &intValue, nil
+}
+
+func GetUUIDQueryParam(request *http.Request, key string) (*uuid.UUID, error) {
+	value := request.URL.Query().Get(key)
+	if value == "" {
+		return nil, nil
+	}
+
+	ID, err := uuid.Parse(value)
+	if err != nil {
+		return nil, fmt.Errorf(
+			"param: %s with key: %s could not be convert to UUID: %v: %w",
+			value, key,
+			err, core_errors.ErrInvalidArgument,
+		)
+	}
+
+	return &ID, nil
+}
+
+func GetLimitOffsetParams(r *http.Request) (*int, *int, error) {
+	limit, err := GetIntQueryParam(r, "limit")
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to get limit query param: %w", err)
+	}
+
+	offset, err := GetIntQueryParam(r, "offset")
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to get offset query param: %w", err)
+	}
+
+	return limit, offset, nil
 }
