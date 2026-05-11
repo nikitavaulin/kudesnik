42398156a6db1e5376b0960753ad3f83d65c770c
nsvaulin <mr.vau2017@mail.ru>
2026-05-12 01:05:45 +0500

    feat: impl jwt

---
+++ internal/core/tools/jwt/jwt.go
@@ -0,0 +1,47 @@
+package tools_jwt
+
+import (
+	"fmt"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type JwtProvider struct {
+	secret []byte
+}
+
+func NewJWTProvider(config Config) *JwtProvider {
+	return &JwtProvider{
+		secret: config.Secret,
+	}
+}
+
+func (j *JwtProvider) GenerateToken(claims *Claims) (string, error) {
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	signedToken, err := jwtToken.SignedString(j.secret)
+	if err != nil {
+		return "", fmt.Errorf("failed to sign jwt token: %w", err)
+	}
+
+	return signedToken, nil
+}
+
+func (j *JwtProvider) DecodeClaims(token string) (*Claims, error) {
+	jwtToken, err := jwt.Parse(token, func(t *jwt.Token) (any, error) {
+		return j.secret, nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse jwt token: %w", err)
+	}
+	if !jwtToken.Valid {
+		return nil, fmt.Errorf("invalid jwt token")
+	}
+
+	claims, ok := jwtToken.Claims.(*Claims)
+	if !ok {
+		return nil, fmt.Errorf("failed to convert claims to MapClaims")
+	}
+
+	return claims, nil
+}
